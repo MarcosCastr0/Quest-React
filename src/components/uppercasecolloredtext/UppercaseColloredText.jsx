3e3c3c1d5d06d727cadf9350e6e81ce88e9c958e
Marcos Castro <117954570+MarcosCastr0@users.noreply.github.com>
2024-09-02 11:28:10 -0400

    Update UppercaseColloredText.jsx

--- src/components/uppercasecolloredtext/UppercaseColloredText.jsx
+++ src/components/uppercasecolloredtext/UppercaseColloredText.jsx
@@ -3,7 +3,6 @@ import "./UppercaseColloredText.css"
 const UppercaseColloredText = (props) =>{
   return (
     <div className="colored-text-container">
-        {/* Armazenando a cor e o estado do paragrafo */}
       <p className="colored-text" style={{ color: props.color }}>
         {props.text.toUpperCase()}
       </p>
